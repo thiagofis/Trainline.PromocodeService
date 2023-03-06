ff8306a895f8aac80f9ab1dae836e298fa971b07
thiago marcolino <thiagomsdev@gmail.com>
2023-03-06 19:46:25 +0000

    #200 | Skeleton campaign endpoint

---
+++ src/Host/Controllers/CampaignController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Trainline.PromocodeService.Contract;
+using Trainline.PromocodeService.Model;
+
+namespace Trainline.PromocodeService.Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CampaignController : ControllerBase
+    {
+        [HttpPost("/get")]
+        public async Task<IActionResult> GetCampaign([FromHeader] Guid campaignId)
+        {
+            // TODO - Campaign service
+            var campaing = new Campaign();
+
+            return Ok(campaing);
+        }
+
+        [HttpPost("/new")]
+        public async Task<IActionResult> NewCampaign([FromBody] NewCampaign request)
+        {
+            // TODO - Campaign service
+            return Ok();
+        }
+
+        [HttpPost("/update")]
+        public async Task<IActionResult> UpdateCampaign([FromBody] UpdateCampaign request)
+        {
+            // TODO - Campaign service
+            return Ok();
+        }
+
+        [HttpPost("/delete")]
+        public async Task<IActionResult> DeleteCampaign([FromHeader] Guid campaignId)
+        {
+            // TODO - Campaign service
+            return Ok();
+        }
+    }
+}
