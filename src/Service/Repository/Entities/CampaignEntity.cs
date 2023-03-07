3cf55504cec082021fe36ad38dc7ae594b68c9d5
thiago marcolino <thiagomsdev@gmail.com>
2023-03-07 12:08:37 +0000

    Added ExpirationDate to CampaignEntity

--- src/Service/Repository/Entities/CampaignEntity.cs
+++ src/Service/Repository/Entities/CampaignEntity.cs
@@ -1,9 +1,12 @@
-﻿namespace Trainline.PromocodeService.Service.Repository.Entities
+﻿using System;
+
+namespace Trainline.PromocodeService.Service.Repository.Entities
 {
     public class CampaignEntity
     {
         public int Id { get; set; }
         public string CampaignId { get; set; }
         public bool Redeemable { get; set; } = true;
+        public DateTime ExpirationDate { get; set; }
     }
 }
